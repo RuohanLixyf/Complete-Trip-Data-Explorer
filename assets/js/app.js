615c205869147df6b1160b963bc842ec272fc6bd
RuohanLixyf <ruohanli373@gmail.com>
2026-01-08 09:28:28 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -155,7 +155,7 @@ let currentViewBounds = null;
         originMarker.bindPopup(
           `
           <b>Trip summary</b><br>
-          Total Distance: ${summary.distanceMile} miles<br>
+          Total Distance: ${summary.distanceMile} mi<br>
           Total Time: ${summary.durationMin} min<br>
           Total Segments: ${summary.segments} <br>
           Start Time: ${summary.startTime} <br>
